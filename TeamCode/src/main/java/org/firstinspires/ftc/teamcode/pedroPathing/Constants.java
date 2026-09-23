064552e72bb347aae62ce3d2fd1889da7270b8d9
Phoenix-2099 <zahid.ma14@gmail.com>
2026-09-23 15:54:23 -0700

    new Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -19,51 +19,5 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
 
-        public static TwoWheelConstants localizerConstants = new TwoWheelConstants()
-                .forwardPodY(1)
-                .strafePodX(1)
-                .forwardEncoder_HardwareMapName("leftFront") // change the name and directions once given
-                .strafeEncoder_HardwareMapName("rightRear")
-                .IMU_HardwareMapName("imu")
-                .forwardPodY(2.5)
-                .strafePodX(.2)
-                .forwardTicksToInches(1)
-                .strafeTicksToInches(1)
 
-
-                .IMU_Orientation(
-                        new RevHubOrientationOnRobot(
-                                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
-                                RevHubOrientationOnRobot.UsbFacingDirection.UP
-                        )
-                );
-
-        public static MecanumConstants driveConstants = new MecanumConstants()
-                .yVelocity(1)
-                .xVelocity(1)
-                .maxPower(1)
-                .rightFrontMotorName("fRM")
-                .rightRearMotorName("bRM")
-                .leftRearMotorName("bLM")
-                .leftFrontMotorName("fLM")
-                .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
-                .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-                .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-                .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
-
-
-        public static FollowerConstants followerConstants = new FollowerConstants().mass(11.3)
-                .lateralZeroPowerAcceleration(1)
-                .forwardZeroPowerAcceleration(1);
-
-        public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
-
-
-        public static Follower createFollower(HardwareMap hardwareMap) {
-        return new FollowerBuilder(followerConstants, hardwareMap)
-                .pathConstraints(pathConstraints)
-                .twoWheelLocalizer(localizerConstants)
-                .mecanumDrivetrain(driveConstants)
-                .build();
-        }
     }
